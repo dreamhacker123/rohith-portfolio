ab4635402318026b5d8a8b2378ad608394d90581
dreamhacker123 <rohithsatyanivas@gmail.com>
2026-03-15 02:51:24 +0530

    fix build

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,9 +1,7 @@
-import { useTheme } from "../api-handlers/themeContext"
 import React from "react"
 
 
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
-  const theme = useTheme();
 
   return (
     <div className={`w-full min-h-screen h-auto m-0 p-0 overflow-x-hidden bg-gradient-to-br from-black via-gray-900 to-black`}>
